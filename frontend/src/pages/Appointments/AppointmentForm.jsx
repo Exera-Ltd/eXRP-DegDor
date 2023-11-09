1570b302370295310e2abf5b2e9e5b2689ba7f90
bilaal-inpleo <bilaal.abdel.hassan@inpleo.com>
2023-11-10 00:25:05 +0400

    Adding Events to Calendar

--- frontend/src/pages/Appointments/AppointmentForm.jsx
+++ frontend/src/pages/Appointments/AppointmentForm.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import { Button, Row, Col, Form, Input, DatePicker, TimePicker, Select } from 'antd';
+import { useState, React } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Row, Col, Form, Input, DatePicker, TimePicker, Select, Modal } from 'antd';
 import moment from 'moment';
+import NewCustomerForm from '../Customers/NewCustomerForm';
 
 const { Option } = Select;
 
-const AppointmentForm = ({ onSubmit }) => {
+const AppointmentForm = ({ onSubmit, slotInfo }) => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [selectedCustomerData, setSelectedCustomerData] = useState(null);
+
+    const showCustomerModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleCustomerModalOk = () => {
+        // Implement functionality for when OK is clicked
+    };
+
+    const handleCustomerModalCancel = () => {
+        setIsModalVisible(false);
+    };
+
     const onFinish = (values) => {
         onSubmit(values); // Pass the form values up to the parent component
     };
@@ -13,237 +30,148 @@ const AppointmentForm = ({ onSubmit }) => {
         console.log('Failed:', errorInfo);
     };
 
+    const selectedStartDate = slotInfo ? moment(slotInfo.start) : null;
+    const selectedEndDate = slotInfo ? moment(slotInfo.end) : null;
+
     return (
-        <Form
-            layout="horizontal"
-            name="appointment-form"
-            labelAlign="left"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item
-                        name="title"
-                        label="Title"
-                        hasFeedback
-                    >
-                        <Select placeholder="Select Title">
-                            <Option value="mr">Mr.</Option>
-                            <Option value="mrs">Mrs.</Option>
-                            <Option value="ms">Ms.</Option>
-                            <Option value="dr">Dr.</Option>
-                            {/* Add more titles as needed */}
-                        </Select>
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="lastName"
-                        label="Last Name"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-            </Row>
+        <>
+            <Form
+                layout="horizontal"
+                name="appointment-form"
+                labelAlign="left"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                initialValues={{
+                    appointmentDate: selectedStartDate ? selectedStartDate : '',
+                    startTime: selectedStartDate,
+                    endTime: selectedEndDate,
+                }}
+                autoComplete="off"
+            >
+                <Row gutter={24}>
+                    <Col span={12}>
+                        <Form.Item
+                            name="customerName"
+                            label="Customer Name"
+                        >
+                            <Input
+                                addonAfter={
+                                    <Button
+                                        icon={<PlusOutlined />} // Replace with your plus icon
+                                        size="small"
+                                        onClick={showCustomerModal}
+                                    />
+                                }
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item
-                        name="customerName"
-                        label="Customer Name"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="city"
-                        label="City"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-            </Row>
+                <Row gutter={24}>
+                    <Col span={12}>
+                        <Form.Item
+                            name="appointmentDate"
+                            label="Appointment Date"
+                        >
+                            <DatePicker.MonthPicker
+                                format="DD-MM-YYYY"
+                                defaultValue={selectedStartDate ? selectedStartDate : ''}
+                            />
+                        </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item
+                            name="status"
+                            label="Status"
+                        >
+                            <Select placeholder="Select Status">
+                                <Option value="scheduled">Scheduled</Option>
+                                <Option value="completed">Completed</Option>
+                                <Option value="noshow">No Show</Option>
+                                <Option value="canceled">Canceled</Option>
+                                {/* Add more statuses as needed */}
+                            </Select>
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item
-                        name="phoneNumber"
-                        label="Phone Number"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="customerAddress"
-                        label="Customer Address"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-            </Row>
+                <Row gutter={24}>
+                    <Col span={12}>
+                        <Form.Item
+                            name="startTime"
+                            label="Start Time"
+                        >
+                            <TimePicker
+                                format="HH:mm"
+                                defaultValue={selectedStartDate}
+                                use12Hours // Remove this line if you want 24-hour format
+                            />
+                        </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item
+                            name="endTime"
+                            label="End Time"
+                        >
+                            <TimePicker
+                                format="HH:mm"
+                                defaultValue={selectedEndDate}
+                                use12Hours // Remove this line if you want 24-hour format
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item
-                        name="mobile1"
-                        label="Mobile 1"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="mobile2"
-                        label="Mobile 2"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-            </Row>
+                <Row gutter={24}>
+                    <Col span={12}>
+                        <Form.Item
+                            name="doctor"
+                            label="Doctor"
+                        >
+                            <Input />
+                        </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item
+                            name="noOfPatients"
+                            label="No. of Patients"
+                        >
+                            <Input />
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item
-                        name="dateOfBirth"
-                        label="Date of Birth"
-                    >
-                        <DatePicker format="DD-MM-YYYY" />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="emailPersonal"
-                        label="Email(personal)"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-            </Row>
+                <Row gutter={24}>
+                    <Col span={24}>
+                        <Form.Item
+                            name="description"
+                            label="Description"
+                        >
+                            <Input.TextArea />
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item
-                        name="emailWork"
-                        label="Email(Work)"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="profession"
-                        label="Profession"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-            </Row>
+                <Row style={{ justifyContent: 'center' }}>
+                    <Button type="primary" htmlType="submit" style={{ width: 200, height: 40 }}>
+                        Add
+                    </Button>
+                </Row>
+            </Form>
 
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item
-                        name="insurance"
-                        label="Insurance"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="customerAge"
-                        label="Customer Age"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-            </Row>
-
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item
-                        name="startEndDate"
-                        label="Start/End Date"
-                    >
-                        <DatePicker.RangePicker format="DD-MM-YYYY" />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="status"
-                        label="Status"
-                    >
-                        <Select placeholder="Select Status">
-                            <Option value="scheduled">Scheduled</Option>
-                            <Option value="completed">Completed</Option>
-                            <Option value="noshow">No Show</Option>
-                            <Option value="canceled">Canceled</Option>
-                            {/* Add more statuses as needed */}
-                        </Select>
-                    </Form.Item>
-                </Col>
-            </Row>
-
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item
-                        name="startTime"
-                        label="Start Time"
-                    >
-                        <TimePicker format="HH:mm" />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="endTime"
-                        label="End Time"
-                    >
-                        <TimePicker format="HH:mm" />
-                    </Form.Item>
-                </Col>
-            </Row>
-
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item
-                        name="doctor"
-                        label="Doctor"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="noOfPatients"
-                        label="No. of Patients"
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-            </Row>
-
-            <Row gutter={24}>
-                <Col span={24}>
-                    <Form.Item
-                        name="description"
-                        label="Description"
-                    >
-                        <Input.TextArea />
-                    </Form.Item>
-                </Col>
-            </Row>
-
-            <Row style={{ justifyContent: 'center' }}>
-                <Button type="primary" htmlType="submit" style={{ width: 200, height: 40 }}>
-                    Add
-                </Button>
-            </Row>
-        </Form>
+            <Modal
+                title="New/ Edit Customer"
+                open={isModalVisible}
+                onOk={handleCustomerModalOk}
+                onCancel={handleCustomerModalCancel}
+                width={1000}
+                footer={null} // Assuming you handle the form submission within the NewCustomerForm
+            >
+                <NewCustomerForm customerData={selectedCustomerData} />
+            </Modal>
+        </>
     );
 };
 
--- frontend/src/pages/Appointments/Calendar.jsx
+++ frontend/src/pages/Appointments/Calendar.jsx
@@ -7,7 +7,7 @@ import AppointmentForm from './AppointmentForm'; // Your previously created form
 
 const Calendar = () => {
     const [modalVisible, setModalVisible] = useState(false);
-    const [events, setEvents] = useState([{title: 'Test', start: '2023-11-07', end: '2023-11-08'}]); // Your events state
+    const [events, setEvents] = useState([{ title: 'Test', start: '2023-11-07', end: '2023-11-08' }]); // Your events state
     const [selectedSlotInfo, setSelectedSlotInfo] = useState(null);
 
     const handleSlotClick = (selectionInfo) => {
@@ -18,15 +18,19 @@ const Calendar = () => {
 
     const handleOk = (formData) => {
         const newEvent = {
-            title: formData.title, // Replace with actual form field names
-            start: selectedSlotInfo.start,
-            end: selectedSlotInfo.end,
+            // Use the description as the title for the event
+            title: formData.customerName + ' | ' + formData.description,
+            start: formData.appointmentDate.format('YYYY-MM-DD') + 'T' + formData.startTime.format('HH:mm:ss'),
+            end: formData.appointmentDate.format('YYYY-MM-DD') + 'T' + formData.endTime.format('HH:mm:ss'),
             // Add other event properties as needed
+            // e.g., extendedProps for additional details
         };
 
+        // Add the new event to the calendar's events state
         setEvents([...events, newEvent]);
         setModalVisible(false);
     };
+
 
     const handleCancel = () => {
         setModalVisible(false);
@@ -50,6 +54,7 @@ const Calendar = () => {
             {modalVisible &&
                 <Modal
                     title="Create Appointment"
+                    width={1000}
                     open={modalVisible}
                     onOk={handleOk} // This needs to be called with form data
                     onCancel={handleCancel}
