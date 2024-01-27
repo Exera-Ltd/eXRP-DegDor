278a03ace804773554b0388ce3cd28674082d853
bilaal-inpleo <bilaal.abdel.hassan@inpleo.com>
2024-01-27 08:21:05 +0400

    Changed API Link

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,2 +1,2 @@
-export const appUrl = 'http://127.0.0.1:8000/';
-//export const appUrl = 'https://kleroptics.exera.mu/';
+//export const appUrl = 'http://127.0.0.1:8000/';
+export const appUrl = 'https://kleroptics.exera.mu/';
