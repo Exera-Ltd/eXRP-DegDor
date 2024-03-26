9949cff72c2fdb054effdf4276908e49bca62a70
bilaal-inpleo <bilaal.abdel.hassan@inpleo.com>
2024-03-26 07:36:18 +0400

    Favicon

--- app_config/views.py
+++ app_config/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from app_config.models import Business
 from app_config.serializers import BusinessSerializer
 from rest_framework import generics
+from django.shortcuts import render
 # Create your views here.
 
 class BusinessListAPIView(generics.ListAPIView):
