2456a2391694fd1bdf9918c356d2a2d54f722fc2
bilaal-inpleo <bilaal.abdel.hassan@inpleo.com>
2024-05-22 15:01:39 +0400

    Built with New URL

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,2 +1,2 @@
-export const appUrl = 'http://127.0.0.1:8000/';
-//export const appUrl = 'https://exrp.alfarglobal.com/';
+//export const appUrl = 'http://127.0.0.1:8000/';
+export const appUrl = 'https://alfarglobal.exrp.mu/';
